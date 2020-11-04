136bf02b05ba6086989920d139b292ce74fea793
ryanchaiyakul <ryanchaiyakul@gmail.com>
2020-11-04 06:26:53 +0000

    fixed bugs with looptest

--- src/main/java/frc/robot/LoopTest.java
+++ src/main/java/frc/robot/LoopTest.java
@@ -4,13 +4,13 @@ import com.team2568.frc2020.loops.Loop;
 
 import edu.wpi.first.wpilibj.Timer;
 
-public class LoopTest extends Loop {
+public class LoopTest implements Loop {
 	public void onStart() {
 		System.out.println("starting my loop");
 	}
 
 	public void onLoop() {
-		double timestamp = Timer.getFPGATimeStamp();
+		double timestamp = Timer.getFPGATimestamp();
 		System.out.println("executing loop at " + timestamp);
 	}
 
