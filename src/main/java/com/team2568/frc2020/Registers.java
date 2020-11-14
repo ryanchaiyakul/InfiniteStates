d1f6941697941ad6d88d1e436394741bdd20a95d
ryanchaiyakul <ryanchaiyakul@gmail.com>
2020-11-14 10:46:34 -0800

    fixed registers by specifying type

--- src/main/java/com/team2568/frc2020/Registers.java
+++ src/main/java/com/team2568/frc2020/Registers.java
@@ -3,5 +3,5 @@ package com.team2568.frc2020;
 import com.team2568.frc2020.registers.Register;
 
 public class Registers {
-	public static final Register kMyReg = new Register<Integer>();
+	public static final Register<Integer> kMyReg = new Register<Integer>();
 }
