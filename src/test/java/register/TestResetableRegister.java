f5f7b288517e1aa9e202fbdad3bc170fa2bb5a1c
viraphol <virapholchaiyakul@gmail.com>
2020-11-28 19:34:40 -0800

    add enumerated register to resetable register tests

--- src/test/java/register/TestResetableRegister.java
+++ src/test/java/register/TestResetableRegister.java
@@ -1,5 +1,3 @@
-
-
 package com.team2568.frc2020.registers;
 
 import com.team2568.frc2020.registers.ResetableRegister;
@@ -12,27 +10,49 @@ import java.util.Random;
 import java.io.*;
 
 
+/**
+ * Test resetable register class
+ * r1 -- register of integer type
+ * r2 -- register of dboule type
+ * r3 -- register of user defined enumerated datatype "Day"
+ */
+
 public class TestResetableRegister {
 
+    // random seed
+    Random rand; 
+
+    // integer register
+    private ResetableRegister<Integer> r1;
+    private int r1_reset_val; 
+
+    // double register
+    private ResetableRegister <Double> r2;
+    private double r2_reset_val; 
 
     // delta use for double comparison, 4 decimal accuracy
     private final double epsilon=0.0001; 
 
-    private ResetableRegister<Integer> r1;
-    private int r1_reset_val; 
+    // enumberated register
+    public enum Day {
+        SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
 
-    private ResetableRegister <Double> r2;
-    private double r2_reset_val; 
+        public static Day nextDay() {
+            Day[] days = Day.values();
+            Random generator = new Random();
+            return days[generator.nextInt(days.length)];
+        }
+    }
+    private ResetableRegister <Day> r3;
+    private Day r3_reset_val;
 
-
-    // random seed
-    Random rand; 
 
     // mimick advance "n" nuber of system clock ticks 
     private void _sysClkTick(int n) {
         for (int i = 0 ; i < n; i++) {
             r1.update();
             r2.update();
+            r3.update();
         }
     }
 
@@ -40,6 +60,7 @@ public class TestResetableRegister {
     private void _sysReset() {
         r1.reset();
         r2.reset();
+        r3.reset();
 
         _sysClkTick(1);
     }
@@ -49,13 +70,14 @@ public class TestResetableRegister {
         System.out.printf("------------------\n"); 
         System.out.printf("r1 reset value=%d\n" , r1_reset_val); 
         System.out.printf("r2 reset value=%f\n" , r2_reset_val); 
+        System.out.printf("r3 reset value=%s\n" , r3_reset_val); 
 
-        //System.out.printf("r1=%d\n" , r1.get().intValue()); 
-        //System.out.printf("r2=%f\n" , r2.get().doubleValue()); 
         System.out.printf("r1 get value=%d\n" , r1.get()); 
         System.out.printf("r2 get value=%f\n" , r2.get()); 
+        System.out.printf("r3 get value=%s\n" , r3.get()); 
 
     }
+
 
 
     @Before
@@ -70,6 +92,8 @@ public class TestResetableRegister {
         r2_reset_val = rand.nextDouble();
         r2 = new ResetableRegister<Double>(r2_reset_val);
 
+        r3_reset_val = Day.nextDay();
+        r3 = new ResetableRegister<Day>(r3_reset_val);
         _sysClkTick(1);
 
     }
@@ -89,18 +113,25 @@ public class TestResetableRegister {
         //_debug();
 
         assertEquals(r1.get().intValue(),r1_reset_val);
+
+        // need epsilon when comparing double value
         assertEquals(r2.get().doubleValue(),r2_reset_val,epsilon);
+
+        assertEquals(r3.get(),r3_reset_val);
     }
 
 
-    // test read-write
-    // write follow by read of 10 random values.  Skip "x" clocks every read
-    // and write (x = random 1--10).
+    /**
+     * Test read-write
+     * write follow by read of 10 random values.  Skip "x" clocks every read
+     * and write (x = random 1--10).
+     */
     @Test
     public void testReadWrite()
     {
         int r1_val;
         double r2_val;
+        Day r3_val;
 
         // reset system first
         _sysReset();
@@ -109,10 +140,12 @@ public class TestResetableRegister {
 
             r1_val = rand.nextInt();
             r2_val = rand.nextDouble();
+            r3_val = Day.nextDay();
 
             // write
             r1.set((Integer)r1_val);
             r2.set((Double)r2_val);
+            r3.set(r3_val);
 
             // skip x-cycle
             _sysClkTick (rand.nextInt(10)+1); 
@@ -122,6 +155,7 @@ public class TestResetableRegister {
             // read
             assertEquals(r1.get().intValue(),r1_val);
             assertEquals(r2.get().doubleValue(),r2_val,epsilon);
+            assertEquals(r3.get(),r3_val);
         }
     }
 
@@ -133,6 +167,9 @@ public class TestResetableRegister {
     {
         int r1_val;
         double r2_val;
+        Day r3_val;
+
+
         int isReset;
 
         // reset system first
@@ -142,11 +179,14 @@ public class TestResetableRegister {
 
             r1_val = rand.nextInt();
             r2_val = rand.nextDouble();
+            r3_val = Day.nextDay();
+
             isReset = rand.nextInt(100)%2; // random
 
             // write
             r1.set((Integer)r1_val);
             r2.set((Double)r2_val);
+            r3.set(r3_val);
 
             // skip x-cycle
             _sysClkTick (rand.nextInt(10)+1); 
@@ -160,10 +200,12 @@ public class TestResetableRegister {
             if (isReset==1) {
                 assertEquals(r1.get().intValue(),r1_reset_val);
                 assertEquals(r2.get().doubleValue(),r2_reset_val,epsilon);
+                assertEquals(r3.get(),r3_reset_val);
 
             } else {
                 assertEquals(r1.get().intValue(),r1_val);
                 assertEquals(r2.get().doubleValue(),r2_val,epsilon);
+                assertEquals(r3.get(),r3_val);
             }
         }
     }
