7041fabf9811f6862694b7d63e7156e58c2ac4ce
viraphol <virapholchaiyakul@gmail.com>
2020-11-28 20:35:37 -0800

    add user define class to resetableRegister test

--- src/test/java/register/TestResetableRegister.java
+++ src/test/java/register/TestResetableRegister.java
@@ -14,7 +14,8 @@ import java.io.*;
  * Test resetable register class
  * r1 -- register of integer type
  * r2 -- register of dboule type
- * r3 -- register of user defined enumerated datatype "Day"
+ * r3 -- register of user defined enumerated type "Day"
+ * r4 -- register of user defined class "Payload"
  */
 
 public class TestResetableRegister {
@@ -33,7 +34,7 @@ public class TestResetableRegister {
     // delta use for double comparison, 4 decimal accuracy
     private final double epsilon=0.0001; 
 
-    // enumberated register
+    // enumerated register
     public enum Day {
         SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
 
@@ -46,6 +47,38 @@ public class TestResetableRegister {
     private ResetableRegister <Day> r3;
     private Day r3_reset_val;
 
+    // user defined class
+    public class Payload {
+        Day data1;
+        int data2;
+        double data3;
+
+        // create payload with random data
+        public Payload (Random generator) {
+            Day[] days = Day.values();
+
+            this.data1 = days[generator.nextInt(days.length)];
+            this.data2 = generator.nextInt();
+            this.data3 = generator.nextDouble();
+        }
+
+        public void _debug () {
+            System.out.printf("     ==> data1 value=%s\n" , data1); 
+            System.out.printf("     ==> data2 value=%d\n" , data2); 
+            System.out.printf("     ==> data3 value=%f\n" , data3); 
+        }
+
+        // return true if data is equal to val
+        public boolean equal(Payload val) {
+            assertEquals(this.data1,val.data1);
+            assertEquals(this.data2,val.data2);
+            assertEquals(this.data3,val.data3,epsilon);
+            return true;
+        }
+    }
+    private ResetableRegister <Payload> r4;
+    private Payload r4_reset_val;
+
 
     // mimick advance "n" nuber of system clock ticks 
     private void _sysClkTick(int n) {
@@ -53,6 +86,7 @@ public class TestResetableRegister {
             r1.update();
             r2.update();
             r3.update();
+            r4.update();
         }
     }
 
@@ -61,6 +95,7 @@ public class TestResetableRegister {
         r1.reset();
         r2.reset();
         r3.reset();
+        r4.reset();
 
         _sysClkTick(1);
     }
@@ -71,11 +106,14 @@ public class TestResetableRegister {
         System.out.printf("r1 reset value=%d\n" , r1_reset_val); 
         System.out.printf("r2 reset value=%f\n" , r2_reset_val); 
         System.out.printf("r3 reset value=%s\n" , r3_reset_val); 
+        System.out.printf("r4 reset value==>  \n"); 
+        r4_reset_val._debug();
 
         System.out.printf("r1 get value=%d\n" , r1.get()); 
         System.out.printf("r2 get value=%f\n" , r2.get()); 
         System.out.printf("r3 get value=%s\n" , r3.get()); 
-
+        System.out.printf("r4 get value==> \n"); 
+        r4.get()._debug();
     }
 
 
@@ -94,6 +132,10 @@ public class TestResetableRegister {
 
         r3_reset_val = Day.nextDay();
         r3 = new ResetableRegister<Day>(r3_reset_val);
+
+        r4_reset_val = new Payload(rand);
+        r4 = new ResetableRegister<Payload>(r4_reset_val);
+
         _sysClkTick(1);
 
     }
@@ -118,6 +160,8 @@ public class TestResetableRegister {
         assertEquals(r2.get().doubleValue(),r2_reset_val,epsilon);
 
         assertEquals(r3.get(),r3_reset_val);
+
+        assertTrue(r4.get().equal(r4_reset_val));
     }
 
 
@@ -132,6 +176,7 @@ public class TestResetableRegister {
         int r1_val;
         double r2_val;
         Day r3_val;
+        Payload r4_val;
 
         // reset system first
         _sysReset();
@@ -141,11 +186,13 @@ public class TestResetableRegister {
             r1_val = rand.nextInt();
             r2_val = rand.nextDouble();
             r3_val = Day.nextDay();
+            r4_val = new Payload(rand);
 
             // write
             r1.set((Integer)r1_val);
             r2.set((Double)r2_val);
             r3.set(r3_val);
+            r4.set(r4_val);
 
             // skip x-cycle
             _sysClkTick (rand.nextInt(10)+1); 
@@ -156,6 +203,7 @@ public class TestResetableRegister {
             assertEquals(r1.get().intValue(),r1_val);
             assertEquals(r2.get().doubleValue(),r2_val,epsilon);
             assertEquals(r3.get(),r3_val);
+            assertTrue(r4.get().equal(r4_val));
         }
     }
 
@@ -168,6 +216,7 @@ public class TestResetableRegister {
         int r1_val;
         double r2_val;
         Day r3_val;
+        Payload r4_val;
 
 
         int isReset;
@@ -180,6 +229,7 @@ public class TestResetableRegister {
             r1_val = rand.nextInt();
             r2_val = rand.nextDouble();
             r3_val = Day.nextDay();
+            r4_val = new Payload(rand);
 
             isReset = rand.nextInt(100)%2; // random
 
@@ -187,10 +237,10 @@ public class TestResetableRegister {
             r1.set((Integer)r1_val);
             r2.set((Double)r2_val);
             r3.set(r3_val);
+            r4.set(r4_val);
 
             // skip x-cycle
             _sysClkTick (rand.nextInt(10)+1); 
-
 
             if (isReset==1) _sysReset();
 
@@ -201,11 +251,12 @@ public class TestResetableRegister {
                 assertEquals(r1.get().intValue(),r1_reset_val);
                 assertEquals(r2.get().doubleValue(),r2_reset_val,epsilon);
                 assertEquals(r3.get(),r3_reset_val);
-
+                assertTrue(r4.get().equal(r4_reset_val));
             } else {
                 assertEquals(r1.get().intValue(),r1_val);
                 assertEquals(r2.get().doubleValue(),r2_val,epsilon);
                 assertEquals(r3.get(),r3_val);
+                assertTrue(r4.get().equal(r4_val));
             }
         }
     }
